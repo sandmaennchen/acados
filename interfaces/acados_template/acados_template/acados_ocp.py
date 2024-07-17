d27a200cc7e5209d6603169da2f7b13a2853e214
sandmaennchen <katrin.baumgaertner@posteo.de>
2024-07-17 17:24:04 +0200

    refactor code_generation cost

--- interfaces/acados_template/acados_template/acados_ocp.py
+++ interfaces/acados_template/acados_template/acados_ocp.py
@@ -968,7 +968,7 @@ class AcadosOcp:
                 generate_c_code_conl_cost(model, stage_type, code_gen_opts)
             elif getattr(self.cost, attr) == 'EXTERNAL':
                 generate_c_code_external_cost(model, stage_type, code_gen_opts)
-
+            # TODO: generic
 
     def remove_x0_elimination(self) -> None:
         self.constraints.idxbxe_0 = np.zeros((0,))
