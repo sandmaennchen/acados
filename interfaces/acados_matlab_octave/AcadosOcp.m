5a33c725d5e517b0b1029d769077c9258d3411e3
Jonathan Frey <jonathanpaulfrey@gmail.com>
2024-09-30 15:37:26 +0200

    Create external function list in code generation (#1271)

--- interfaces/acados_matlab_octave/AcadosOcp.m
+++ interfaces/acados_matlab_octave/AcadosOcp.m
@@ -48,6 +48,8 @@ classdef AcadosOcp < handle
         name
         zoro_description
         casadi_pool_names
+        external_function_files_ocp
+        external_function_files_model
     end
     methods
         function obj = AcadosOcp()
@@ -825,6 +827,7 @@ classdef AcadosOcp < handle
             if strcmp(ocp.model.dyn_ext_fun_type, 'generic')
                 check_dir_and_create(model_dir);
                 copyfile(fullfile(pwd, ocp.model.dyn_generic_source), model_dir);
+                context.add_external_function_file(ocp.model.dyn_generic_source, model_dir);
             elseif strcmp(ocp.model.dyn_ext_fun_type, 'casadi')
                 check_casadi_version();
                 switch solver_opts.integrator_type
@@ -858,7 +861,7 @@ classdef AcadosOcp < handle
             for i = 1:3
                 if strcmp(cost_ext_fun_types{i}, 'generic')
                     if strcmp(cost_types{i}, 'EXTERNAL')
-                        setup_generic_cost(cost, cost_dir, stage_types{i})
+                        setup_generic_cost(context, cost, cost_dir, stage_types{i})
                     else
                         error('Unknown cost_type for cost_ext_fun_types generic: got %s', cost_types{i});
                     end
